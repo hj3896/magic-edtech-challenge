0aeec491f1c1565a0e2cadd938143e85ffbcb920
Admin <rishav.saxena9412@gmail.com>
2025-04-27 00:16:29 +0530

    Create default user

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,6 +2,8 @@ import dotenv from "dotenv";
 import connectDB from "./configs/db";
 import app from "./app";
 import { log, setupLogging } from "./configs/logger";
+import user from "./models/user";
+import bcrypt from "bcryptjs";
 
 dotenv.config();
 
@@ -17,3 +19,21 @@ connectDB()
   .catch((err) => {
     log.error("DB connection failed:", err);
   });
+
+async function init() {
+  let password = await bcrypt.hash("password", 10);
+  user
+    .findOneAndUpdate(
+      { username: "admin" },
+      {
+        username: "admin",
+        password: password,
+        role: "admin",
+      },
+      { upsert: true }
+    )
+    .then((val) => {
+      log.info("Admin Document Inserted");
+    });
+}
+init();
